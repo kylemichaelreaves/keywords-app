65b975d35743fc99f2d786a9a253b7dbd181a75b
Kyle <kylereaves@gmail.com>
2023-03-21 15:50:04 -0400

    Vitest testing oh my

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -5,6 +7,10 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+  },
   server: {
     host: '127.0.0.1',
     port: 5173
